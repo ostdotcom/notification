4fe7b39480e85891e69bd8ed4dcf74226440d646
santhu210 <santhosh.nitdgp2015@gmail.com>
2018-07-25 15:11:36 +0530

    Testing prettier

--- test_subscribe.js
+++ test_subscribe.js
@@ -1,28 +1,30 @@
-"use strict";
+'use strict';
 
 const openSTNotification = require('./index');
 
-const rootPrefix = ''
-  , logger = require(rootPrefix + '/lib/logger/custom_console_logger')
-;
+const rootPrefix = '',
+  logger = require(rootPrefix + '/lib/logger/custom_console_logger');
 
 var unAckCount = 0;
 
-openSTNotification.subscribeEvent.rabbit(["ackqueue.test"], {queue: 'myQueue', ackRequired: 1, prefetch:10}, function(msgContent){
-  unAckCount++;
-  return new Promise(function(onResolve, onReject){
-    logger.debug('Consumed message -> ', msgContent);
-    setTimeout(function(){
-      unAckCount--;
-      onResolve();
-    }, 3000)
-  });
+openSTNotification.subscribeEvent.rabbit(
+  ['ackqueue.test'],
+  { queue: 'myQueue', ackRequired: 1, prefetch: 10 },
+  function(msgContent) {
+    unAckCount++;
+    return new Promise(function(onResolve, onReject) {
+      logger.debug('Consumed message -> ', msgContent);
+      setTimeout(function() {
+        unAckCount--;
+        onResolve();
+      }, 3000);
+    });
+  }
+);
 
-});
-
-process.on('SIGINT', function () {
+process.on('SIGINT', function() {
   logger.debug('Received SIGINT, checking unAckCount.');
-  var f = function(){
+  var f = function() {
     if (unAckCount === 0) {
       process.exit(1);
     } else {
